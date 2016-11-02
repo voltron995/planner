5f92cbe7899ab2ea7322cb79fe9f7b2c1973d307
kate <kate.glebova97@gmail.com>
2016-11-02 15:15:16 +0200

    Make simple config

--- backend/config/config.py
+++ backend/config/config.py
@@ -1,6 +1,38 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__) + '/../../')
 
-DEBUG = False
 TESTING = False
 CSRF_ENABLED = True
+DEVELOPMENT = True
+DEBUG = True
+config_dict = {
+        "version": 1,
+        "formatters": {
+            "brief": {"format": "%(levelname)-8s: %(name)-15s: %(message)s"},
+            "full": {"format": "%(asctime)s %(name)-15s %(levelname)-8s %(message)s"}
+        },
+        "handlers": {
+            "filelog": {
+                "class": "logging.handlers.RotatingFileHandler",
+                "formatter": "full",
+                "level": "INFO",
+                "filename": basedir + '/log/planner.log'},
+            "console": {
+                "class":"logging.StreamHandler",
+                "formatter": "brief",
+                "level": "WARN",
+                "stream": "ext://sys.stderr"}
+        },
+        "root": {"level": "DEBUG", "handlers": ["filelog", "console"]}
+    }
+logging.config.dictConfig(config_dict)
+
+DB_CONFIG = {
+   'DRIVER': '',
+   'USER': '',
+   'PASSWORD': '',
+   'HOST': '',
+   'PORT': 5342,
+   'NAME': ''}
+SQLALCHEMY_DATABASE_URI = '{DRIVER}://{USER}:{PASSWORD}@{HOST}:{PORT}/{NAME}'.format(**DB_CONFIG)
+
