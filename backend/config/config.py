1daeb3c8b19c4359046c9229550f08937f756c47
Dmytro Yefremov <efremov.dma@gmail.com>
2016-11-03 09:40:06 +0000

    Fixed formatting.

--- backend/config/config.py
+++ backend/config/config.py
@@ -1,5 +1,6 @@
 import os
 import logging.config
+
 basedir = os.path.abspath(os.path.dirname(__file__) + '/../../')
 
 TESTING = False
@@ -7,33 +8,32 @@ CSRF_ENABLED = True
 DEVELOPMENT = True
 DEBUG = True
 config_dict = {
-        "version": 1,
-        "formatters": {
-            "brief": {"format": "%(levelname)-8s: %(name)-15s: %(message)s"},
-            "full": {"format": "%(asctime)s %(name)-15s %(levelname)-8s %(message)s"}
-        },
-        "handlers": {
-            "filelog": {
-                "class": "logging.handlers.RotatingFileHandler",
-                "formatter": "full",
-                "level": "INFO",
-                "filename": basedir + '/log/planner.log'},
-            "console": {
-                "class":"logging.StreamHandler",
-                "formatter": "brief",
-                "level": "WARN",
-                "stream": "ext://sys.stderr"}
-        },
-        "root": {"level": "DEBUG", "handlers": ["filelog", "console"]}
-    }
+    "version": 1,
+    "formatters": {
+        "brief": {"format": "%(levelname)-8s: %(name)-15s: %(message)s"},
+        "full": {"format": "%(asctime)s %(name)-15s %(levelname)-8s %(message)s"}
+    },
+    "handlers": {
+        "filelog": {
+            "class": "logging.handlers.RotatingFileHandler",
+            "formatter": "full",
+            "level": "INFO",
+            "filename": basedir + '/log/planner.log'},
+        "console": {
+            "class": "logging.StreamHandler",
+            "formatter": "brief",
+            "level": "WARN",
+            "stream": "ext://sys.stderr"}
+    },
+    "root": {"level": "DEBUG", "handlers": ["filelog", "console"]}
+}
 logging.config.dictConfig(config_dict)
 
 DB_CONFIG = {
-   'DRIVER': '',
-   'USER': '',
-   'PASSWORD': '',
-   'HOST': '',
-   'PORT': 5342,
-   'NAME': ''}
+    'DRIVER': '',
+    'USER': '',
+    'PASSWORD': '',
+    'HOST': '',
+    'PORT': 5342,
+    'NAME': ''}
 SQLALCHEMY_DATABASE_URI = '{DRIVER}://{USER}:{PASSWORD}@{HOST}:{PORT}/{NAME}'.format(**DB_CONFIG)
-
