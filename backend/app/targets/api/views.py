297752482c531e7ef3f7802ede4bf26d1afaa162
Dmytro Yefremov <efremov.dma@gmail.com>
2016-11-25 11:51:36 +0000

    fixed views for targets.

--- backend/app/targets/api/views.py
+++ backend/app/targets/api/views.py
@@ -1,43 +1,41 @@
-from flask_login import current_user
-from flask.views import MethodView
 from flask import request
-from app.api import response
+from flask.views import MethodView
+from flask_login import current_user
+
 from app import db
-import json
+from app.api import response
 from app.targets.api.schemas import TargetSchema
 from app.targets.models import Target
-
-OK = json.dumps({"OK": 200})
 
 
 class TargetsList(MethodView):
 
     def get(self):
-        targets = current_user.targets
-        return response.success(data=targets, schema=TargetSchema, many=True)
+        return response.success(data=current_user.targets, schema=TargetSchema, many=True)
 
     def post(self):
-        target_data = request.json.get("data").get("attributes")
-        target_data['user_id'] = current_user.id
-        new_target = Target(**target_data)
-        db.session.add(new_target)
+        data = request.json
+        data['user_id'] = current_user.id
+
+        target = Target(**data)
+        db.session.add(target)
         db.session.commit()
-        return response.success(data=new_target, schema=TargetSchema)
+
+        return response.success(data=target, schema=TargetSchema)
 
 
 class TargetSingle(MethodView):
-    def get(self, target_uuid):
-        target_data = Target.query.filter_by(uuid=target_uuid).first()
-        return response.success(data=target_data, schema=TargetSchema)
+    def get(self, id):
+        return response.success(data=Target.query.get(id), schema=TargetSchema)
 
-    def put(self, target_uuid):
-        target_data = Target.query.filter_by(uuid=target_uuid).first()
-        target_data.query.update(request.json["data"]["attributes"])
+    def put(self, id):
+        target = Target.query.get(id)
+        target.query.update(request.json)
         db.session.commit()
-        return response.success(data=target_data, schema=TargetSchema)
+        return response.success(data=target, schema=TargetSchema)
 
-    def delete(self, target_uuid):
-        target_to_delete = Target.query.filter_by(uuid=target_uuid).first()
-        db.session.delete(target_to_delete)
+    def delete(self, id):
+        target = Target.query.get(id)
+        db.session.delete(target)
         db.session.commit()
-        return OK
+        return response.success()
