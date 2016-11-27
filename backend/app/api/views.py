756b125a26966aeacb1e27afd5dac1c9b65442c3
Dmytro Yefremov <efremov.dma@gmail.com>
2016-11-27 11:14:32 +0000

    abstract attributes for base view.

--- backend/app/api/views.py
+++ backend/app/api/views.py
@@ -4,17 +4,21 @@ from flask.views import MethodView
 
 from app import db
 from app.api import response
+from app.api.schemas import ModelSchema
 from app.errors import BadRequest, InvalidAttribute, Error
+from app.models import BaseModel
 
 
 class BaseView(MethodView):
+    model = NotImplementedError  # type: BaseModel
+    schema = NotImplementedError  # type: ModelSchema
+
     def _validate_schema(self, partial=None):
         """
-        :param schema: ModelSchema.__class__
         :param bool|tuple partial: Whether to ignore missing fields. If its value is an iterable,
             only missing fields listed in that iterable will be ignored.
         """
-        errors = self.schema().validate(self._parse_json(), partial=partial)
+        errors = self.schema().validate(self.__parse_json(), partial=partial)
         if errors:
             exception = BadRequest()
             for attr, messages in errors.items():
@@ -22,7 +26,7 @@ class BaseView(MethodView):
                     exception.add_error(InvalidAttribute(source=attr, detail=msg))
             raise exception
 
-    def _parse_json(self):
+    def __parse_json(self):
         json = {}
         if request.is_json:
             try:
