58edc07831fac6d70ad415763e33f13af09fb4fe
Dmytro Yefremov <efremov.dma@gmail.com>
2016-12-05 12:44:52 +0000

    use npm copy plugin to copy assests.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -3,10 +3,6 @@ import {Component} from '@angular/core';
 // todo: do not import assets here, figure out how to compile them without import.
 import '../../public/css/styles.css'
 import '../../public/css/forms.css'
-import '../../public/images/avatar-default.png'
-import '../../public/images/logo.png'
-import '../../public/images/sorry.jpg'
-import '../../public/images/404.gif'
 
 @Component({
     selector: 'planner-app',
