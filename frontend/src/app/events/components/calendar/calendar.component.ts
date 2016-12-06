14eb27e5a8cfe48a81dc461d2ff0e65aaab6af3d
dvytvtc <dimavitvickiy@gmail.com>
2016-12-06 17:59:52 +0200

    add calendar and fixed buggs

--- frontend/src/app/events/components/calendar/calendar.component.ts
+++ frontend/src/app/events/components/calendar/calendar.component.ts
@@ -1,15 +1,139 @@
-import {Component, OnInit, OnDestroy} from '@angular/core';
+import { Component, Input } from '@angular/core';
+import {
+  startOfDay,
+  subDays,
+  addDays,
+  endOfMonth,
+  isSameDay,
+  isSameMonth,
+  addWeeks,
+  subWeeks,
+  addMonths,
+  subMonths,
+  addHours
+} from 'date-fns';
+import { Subject } from 'rxjs/Subject';
+import {
+  CalendarEvent,
+  CalendarEventAction,
+  CalendarEventTimesChangedEvent
+} from 'angular-calendar'; // import should be from `angular-calendar` in your app
 
+const colors: any = {
+  red: {
+    primary: '#ad2121',
+    secondary: '#FAE3E3'
+  },
+  blue: {
+    primary: '#1e90ff',
+    secondary: '#D1E8FF'
+  },
+  yellow: {
+    primary: '#e3bc08',
+    secondary: '#FDF1BA'
+  }
+};
 
 @Component({
-    selector: 'calendar',
+  selector: 'calendar',
+  styles: [`
+    h3 {
+      margin: 0;
+    }
+    .container {
+      padding-bottom: 50px;
+    }
+  `],
     templateUrl: 'calendar.component.html',
-    styleUrls: [
-        'calendar.component.css'
-    ],
-
 })
 
+
 export class CalendarComponent {
-    calendarTest: "calendar"
+
+  @Input()
+  eventsList: any[];
+
+  view: string = 'month';
+
+  viewDate: Date = new Date();
+
+  actions: CalendarEventAction[] = [{
+    label: '<i class="fa fa-fw fa-pencil"></i>',
+    onClick: ({event}: {event: CalendarEvent}): void => {
+      console.log('Edit event', event);
+    }
+  }, {
+    label: '<i class="fa fa-fw fa-times"></i>',
+    onClick: ({event}: {event: CalendarEvent}): void => {      
+      this.events = this.events.filter(iEvent => iEvent !== event);
+    }
+  }];
+
+  refresh: Subject<any> = new Subject();
+
+  events: CalendarEvent[] = [];
+
+  activeDayIsOpen: boolean = true;
+
+  increment(): void {
+
+    const addFn: any = {
+      day: addDays,
+      week: addWeeks,
+      month: addMonths
+    }[this.view];
+
+    this.viewDate = addFn(this.viewDate, 1);
+
+  }
+
+  decrement(): void {
+
+    const subFn: any = {
+      day: subDays,
+      week: subWeeks,
+      month: subMonths
+    }[this.view];
+
+    this.viewDate = subFn(this.viewDate, 1);
+
+  }
+
+  today(): void {
+    this.viewDate = new Date();
+  }
+
+  dayClicked({date, events}: {date: Date, events: CalendarEvent[]}): void {
+
+    if (isSameMonth(date, this.viewDate)) {
+      if (
+        (isSameDay(this.viewDate, date) && this.activeDayIsOpen === true) ||
+        events.length === 0
+      ) {
+        this.activeDayIsOpen = false;
+      } else {
+        this.activeDayIsOpen = true;
+        this.viewDate = date;
+      }
+    }
+  }
+
+  eventTimesChanged({event, newStart, newEnd}: CalendarEventTimesChangedEvent): void {
+    event.start = newStart;
+    event.end = newEnd;
+    this.refresh.next();
+  }
+
+  ngAfterContentInit() {
+      for (var event of this.eventsList) {
+          var eventObj = {
+              start: new Date(event.startTime),
+              end: new Date(event.endTime),
+              title: event.name,
+              color: colors.yellow,
+              actions: this.actions
+          }
+          this.events.push(eventObj)
+      };
+  }
 }
