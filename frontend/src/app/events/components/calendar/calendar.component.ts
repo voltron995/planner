b9648e8e33893ccdaaafce3f9406baaa82974797
dvytvtc <dimavitvickiy@gmail.com>
2016-12-07 17:06:23 +0200

    add events view and edit

--- frontend/src/app/events/components/calendar/calendar.component.ts
+++ frontend/src/app/events/components/calendar/calendar.component.ts
@@ -32,9 +32,15 @@ const colors: any = {
     primary: '#1e90ff',
     secondary: '#D1E8FF'
   },
+
   yellow: {
     primary: '#e3bc08',
     secondary: '#FDF1BA'
+  }, 
+
+  green: {
+      primary: '#21b319',
+      secondary: '#458c41'
   }
 };
 
@@ -143,7 +149,7 @@ export class CalendarComponent {
               start: new Date(event.startTime),
               end: new Date(event.endTime),
               title: event.name,
-              color: colors.yellow,
+              color: colors.green,
               actions: this.actions,
               cssClass: event.id
           }
