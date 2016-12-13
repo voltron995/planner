fd0751b28bfeed91d5d62e0eafc21d4acddca1dd
Dmytro Yefremov <efremov.dma@gmail.com>
2016-12-13 09:36:56 +0000

    refs #72: images for recipes.

--- backend/app/plugins/recipes/recipes/schemas.py
+++ backend/app/plugins/recipes/recipes/schemas.py
@@ -1,5 +1,9 @@
+from flask import url_for
 from marshmallow import Schema, fields
+from marshmallow import post_dump
 
+from app.uploads import groups
+from app.uploads.manager import UploadsManager
 from ..ingredients.schemas import IngredientListSchema
 
 
@@ -15,7 +19,22 @@ class RecipeSchema(Schema):
     img_path = fields.String()
     categories = fields.Nested(CategorySchema, many=True)
     ingredients = fields.Nested(
-    	IngredientListSchema,
-    	many=True,
+        IngredientListSchema,
+        many=True,
         required=True)
     price = fields.String(dump_only=True)
+
+    @post_dump(pass_many=True)
+    def post_dump_callback(self, data, many):
+        if not many:
+            return self._add_image_link(data)
+        else:
+            return [self._add_image_link(item) for item in data]
+
+    # todo: do it better
+    def _add_image_link(self, item):
+        if item['img_path']:
+            item['image_link'] = UploadsManager.get_link(item['img_path'], groups.RECIPE_IMAGES)
+        else:
+            item['image_link'] = url_for('static', filename='assets/recipe-default.jpg')
+        return item
--- backend/app/uploads/groups.py
+++ backend/app/uploads/groups.py
@@ -1,2 +1,3 @@
 TEMPORARY_FILES = 'tmp'
 PROFILE_IMAGES = 'profile-images'
+RECIPE_IMAGES = 'recipe-images'
