6327fdf66de168a0d0a2c52686de787840faa25b
Dmytro Yefremov <efremov.dma@gmail.com>
2016-12-14 22:27:51 +0000

    merge feature/60-errors_handler into development.

--- backend/app/error_handlers/__init__.py
+++ backend/app/error_handlers/__init__.py
@@ -3,4 +3,4 @@ from app.error_handlers.error_handlers import http_error_handler
 from app.error_handlers.errors import HTTP_ERROR_CODES
 
 for code in HTTP_ERROR_CODES:
-    app.errorhandler(code)(http_error_handler)
+    app.register_error_handler(code, http_error_handler)
