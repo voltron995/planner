06d7df8a1fe1c363c3cc528dae18197bc2599475
dimavitvickiy <dimavitvickiy@gmail.com>
2016-12-21 15:24:32 +0200

    add delete-orpham to events and items

--- backend/app/items/models.py
+++ backend/app/items/models.py
@@ -9,6 +9,6 @@ class Item(BaseModel):
     __tablename__ = 'items'
 
     event_id = db.Column(UUID, db.ForeignKey('events.id'), nullable=False)
-    event = relationship('Event', cascade='all, delete-orphan', backref='items')
+    event = relationship("Event", back_populates="items")
     plugin = db.Column(db.String, nullable=False)
     plugin_item_id = db.Column(db.Integer, nullable=False)
